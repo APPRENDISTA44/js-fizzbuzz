2ddd5d43e44690a8e12e47b4a3c9fc7f31763dcf
Luca Isidoro <apprendista44@gmail.com>
2020-05-27 15:54:32 +0200

    aggiunti commenti mancanti

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
+//mi aggancio all'html
 var risposta = document.getElementById('risposta');
+//ciclo for per scandire i 100 numeri, con gli if valuto cosa stampare
 for (var i = 1; i<= 100; i++) {
  if ( (i % 3 == 0) && (i % 5 == 0) ) {
    risposta.innerHTML += '  <li> FizzBuzz </li>';
